cb6267b07c54cc5594e95e41b0c93147117e5c78
MisticNom <mihaibarbu@outlook.com>
2017-03-24 11:32:40 +0200

    buton stergere

--- Proiect/Proiect/Form1.Designer.cs
+++ Proiect/Proiect/Form1.Designer.cs
@@ -45,23 +45,23 @@
             this.labelVarsta = new System.Windows.Forms.Label();
             this.textBoxVarsta = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBoxIesire = new System.Windows.Forms.PictureBox();
             this.pictureBoxAfisare = new System.Windows.Forms.PictureBox();
             this.pictureBoxStergere = new System.Windows.Forms.PictureBox();
             this.pictureBoxInserare = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.panel2 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIesire)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAfisare)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxStergere)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxInserare)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -69,9 +69,9 @@
             // 
             this.butonInserare.BackColor = System.Drawing.Color.WhiteSmoke;
             this.butonInserare.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.butonInserare.Location = new System.Drawing.Point(387, 265);
+            this.butonInserare.Location = new System.Drawing.Point(387, 267);
             this.butonInserare.Name = "butonInserare";
-            this.butonInserare.Size = new System.Drawing.Size(124, 31);
+            this.butonInserare.Size = new System.Drawing.Size(123, 30);
             this.butonInserare.TabIndex = 7;
             this.butonInserare.Text = "Inserare";
             this.butonInserare.UseVisualStyleBackColor = false;
@@ -170,7 +170,7 @@
             // 
             this.butonSterge.BackColor = System.Drawing.Color.WhiteSmoke;
             this.butonSterge.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.butonSterge.Location = new System.Drawing.Point(387, 265);
+            this.butonSterge.Location = new System.Drawing.Point(387, 266);
             this.butonSterge.Name = "butonSterge";
             this.butonSterge.Size = new System.Drawing.Size(123, 31);
             this.butonSterge.TabIndex = 9;
@@ -241,6 +241,56 @@
             this.panel1.Size = new System.Drawing.Size(200, 305);
             this.panel1.TabIndex = 17;
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(15, 270);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 21);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Inchide";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 21);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Afiseaza date";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 21);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Sterge pacient";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 21);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Adauga pacient";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(143, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(54, 50);
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
             // pictureBoxIesire
             // 
             this.pictureBoxIesire.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
@@ -303,16 +353,6 @@
             this.pictureBoxInserare.MouseEnter += new System.EventHandler(this.pictureBoxInserare_MouseEnter);
             this.pictureBoxInserare.MouseLeave += new System.EventHandler(this.pictureBoxInserare_MouseLeave);
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox1.Location = new System.Drawing.Point(143, 3);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(54, 50);
-            this.pictureBox1.TabIndex = 18;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
-            // 
             // timer1
             // 
             this.timer1.Interval = 10;
@@ -340,46 +380,6 @@
             this.panel2.Size = new System.Drawing.Size(525, 302);
             this.panel2.TabIndex = 18;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 88);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(117, 21);
-            this.label1.TabIndex = 19;
-            this.label1.Text = "Adauga pacient";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 135);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(108, 21);
-            this.label2.TabIndex = 20;
-            this.label2.Text = "Sterge pacient";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(12, 182);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(101, 21);
-            this.label3.TabIndex = 21;
-            this.label3.Text = "Afiseaza date";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(15, 270);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(60, 21);
-            this.label4.TabIndex = 22;
-            this.label4.Text = "Inchide";
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -399,11 +399,11 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIesire)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAfisare)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxStergere)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxInserare)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.ResumeLayout(false);
